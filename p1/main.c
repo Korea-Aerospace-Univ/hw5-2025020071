eb32a9631daa2d691c2d3f5a5df1b4da7a8b81bf
2025020071 <sonjiho2006@gmail.com>
2026-05-04 15:42:27 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N, m = 0;
+    
+    scanf("%d", &N);
+    
+    for(int i=1; i<=N/900; i++) {
+        for(int j=2; j<=N/750; j+=2) {
+            for(int k=1; k<=N/200; k++) {
+                if ((900*i+750*j+200*k == N) && (k<i || k<j)) {
+                    printf("%d %d %d\n", i, j, k);
+                    m = 1;
+                    
+                }
+            }
+        }
+    }
+    if (m=0) 
+        printf("none");
+        
+    return 0;
+}
